fad5d0797ce0e52f59c3afeecab28d3bf9579c4e
Franco Vilte <francovilte1994@gmail.com>
2023-08-07 22:13:00 -0300

    se modifica el menu y se distancian las fichas 5vh

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
+const menu = document.querySelector('nav');
 const menuImagen = document.querySelector('.menuImagen');
+const menuHambur = document.querySelector('#menuHambur');
 const menuMobile = document.querySelector('.menuMobile');
 
 menuImagen.addEventListener('click', toggleMenuMobile);
 
-function toggleMenuMobile(){        
+function toggleMenuMobile(){          
     menuMobile.classList.toggle('inactivo')
+
+    if(!menuMobile.classList.contains('inactivo')){
+        menu.style.background = '#680707'        
+        menuHambur.classList.remove('fa-bars')
+        menuHambur.classList.add('fa-times')
+    }else{
+        menu.style.background = 'transparent'        
+        menuHambur.classList.remove('fa-times')
+        menuHambur.classList.add('fa-bars')
+    }
+
+    console.log(menuHambur) 
 }
